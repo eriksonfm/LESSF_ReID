caf1991e72c2a17ec4ddefff6442ca860e900132
erikson <eriksonfm@gmail.com>
2024-07-09 10:10:49 -0300

    Resultado usando todos os modelos com rgb crop no rosto, sem ruido e sem ward no agglomerative

--- datasetUtils.py
+++ datasetUtils.py
@@ -107,11 +107,11 @@ def load_dataset(dataset_name, use_ruido=False):
 		queries_images = load_set_from_MSMT17("/hadatasets/ReID_Datasets/MSMT17_V2/list_query.txt", base_name_test)
   
 	elif dataset_name == "Jadson":
-		base_name_dir = "/hadatasets/Synthetic-Realities/20-spoofing-mpad/2020-plosone-recod-mpad"
+		base_name_dir = "/hadatasets/Synthetic-Realities/20-spoofing-mpad/2020-plosone-recod-mpad/crops/"
 
-		train_images = load_from_Jadson("csvs/train_motog5.csv", base_name_dir, use_ruido) 
-		gallery_images = load_from_Jadson("csvs/test_motog5.csv", base_name_dir, use_ruido) 
-		queries_images = load_from_Jadson("csvs/val_motog5.csv", base_name_dir, use_ruido) 
+		train_images   = load_from_Jadson("csvs/mpad_crop_just_motog_train.csv", base_name_dir, use_ruido) 
+		gallery_images = load_from_Jadson("csvs/mpad_crop_just_motog_test.csv",  base_name_dir, use_ruido) 
+		queries_images = load_from_Jadson("csvs/mpad_crop_just_motog_val.csv",   base_name_dir, use_ruido) 
 
 
 	return train_images, gallery_images, queries_images
